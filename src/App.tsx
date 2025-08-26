d956be52c0adff5c0435d0c289d0482faaf6836a
Henrique-CBueno <henriquecb2507@outlook.pt>
2025-08-26 18:41:00 -0300

    aside sketch

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
+import Aside from "./components/aside";
+
 function App() {
+  const [activeField, setActiveField] = useState<number | null>(2);
+
   return(
-    <div>
-      
+    <div className="h-screen w-screen grid grid-cols-[5%_1fr] dark:bg-[#F2F2F2]">
+      <aside className="">
+        <Aside activeField={activeField} setActiveField={setActiveField} />
+      </aside>
+
+      <main className="bg-green-50 p-8">
+        <h1>{activeField}</h1>
+      </main>
+
     </div>
   )
 }
